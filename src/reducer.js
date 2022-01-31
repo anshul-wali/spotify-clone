913e73a04c34985d9cea145b39803e573172105e
anshul-wali <91263826+anshul-wali@users.noreply.github.com>
2022-01-31 15:18:13 +0530

    Update reducer.js

--- src/reducer.js
+++ src/reducer.js
@@ -3,8 +3,6 @@ export const initialState = {
   playlist: [],
   playing: false,
   item: null,
-  //remove after developing
-  // token : "BQB9PFlXB_hxVYksu5h9RzWPfa2H66a8pzIntw59Fa9uIO8ANhuuidk629LvJ9CGsON5OmMtb2bZO4FqFtrZoZuD4dZVf7E34NL-wjmVXXWEG4hCycIR-DTQh9BlKbiOHKV4gDe_1UY2Ij1aFuqLtiZyQzP1b_blXD_RDu_UzRoENDkOKTHu"
 };
 
 const reducer = (state, action) => {
